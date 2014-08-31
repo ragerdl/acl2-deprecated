e8883f4040a03fc4509f62bbf2b91bbebc377dc6
ragerdl <ragerdl@gmail.com>
2014-08-31 16:30:04 -0500

    Update build-ccl-acl2h.sh

--- books/build/jenkins/build-ccl-acl2h.sh
+++ books/build/jenkins/build-ccl-acl2h.sh
@@ -22,8 +22,11 @@ startjob -c "make acl2h LISP=$LISP &> make.log" \
   --limits "pmem=4gb,nodes=1:ppn=1,walltime=10:00"
 
 echo "Building the books."
-cd acl2-devel/books
-make ACL2=$ACL2DIR/acl2-devel/saved_acl2h all $MAKEOPTS USE_QUICKLISP=1
+cd books
+make ACL2=saved_acl2h all $MAKEOPTS USE_QUICKLISP=1
+
+#cd acl2-devel/books
+#make ACL2=$ACL2DIR/acl2-devel/saved_acl2h all $MAKEOPTS USE_QUICKLISP=1
 
 echo "Build was successful."
 
