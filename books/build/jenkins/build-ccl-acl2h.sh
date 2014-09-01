de5229d28b45f017388141c61ba99752dc20bf2e
David L Rager <ragerdl@gmail.com>
2014-08-31 20:24:20 -0500

    Fixing up jenkins script

--- books/build/jenkins/build-ccl-acl2h.sh
+++ books/build/jenkins/build-ccl-acl2h.sh
@@ -17,7 +17,7 @@ echo "Using LISP = $LISP"
 echo "Using STARTJOB = `which startjob`"
 
 echo "Making ACL2(h)"
-startjob -c "make acl2h LISP=$LISP &> make.log" \
+startjob -c "make ACL2_HONS=t LISP=$LISP &> make.log" \
   --name "J_CCL_ACL2H" \
   --limits "pmem=4gb,nodes=1:ppn=1,walltime=10:00"
 
